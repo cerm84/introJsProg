acedf9ab4034996ec5c26c47f8942007cc69c5e4
Carlos Romero Mejía <cerm84@gmail.com>
2017-08-07 17:10:39 -0600

    Add functions and constructors to the project

--- JS102-master/scripts.js
+++ JS102-master/scripts.js
@@ -54,3 +54,70 @@ for (var i = 0; i < animals.length; i++){
 };
 
 console.log(animals);
+
+//resives a username and return and return an objectwith taht name and with an array if there are more tahn one argument
+var animalTestUser = function(uname) {
+    var unameObject = {};
+    unameObject.uname = uname;
+    var otherArgs = [];
+    if (arguments.length > 0) {
+        for (var i = 1; i < arguments.length; i++) {
+            otherArgs.push(arguments[i]);
+        }
+    }
+    unameObject.otherArgs = otherArgs;
+    return unameObject;
+}
+
+
+// creates an object with "animals" properties
+var AnimalCreator = function(username, species, tagline, noises) {
+    return {
+        username: username,
+        species: species,
+        tagline: tagline,
+        noises: noises,
+        friends: []
+    };
+};
+
+//fill the objects.friends propertie array with the name propertie of another "animal"
+var addFriend = function(user, friend) {
+    user['friends'].push(friend.username);
+}
+
+//
+var myFarm = [
+    cow= AnimalCreator('Lechera', 'cow', 'No soy una vaca cualquiera', ['moohhh', 'moo', 'meuh', 'ammuh']),
+    horse = AnimalCreator('Silver', 'horse', 'Hi Yo', ['Neig', 'Vrinsk', 'I-Haa', 'ihhh']),
+    cat = AnimalCreator('Felix', 'cat', 'Hello officer', ['meow', 'miau', 'purr', 'miao']),
+    dog = AnimalCreator('Laika', 'dog', 'Im a space dog', ['guau', 'woof', 'wong', 'barf']),
+    duck = AnimalCreator('Daffie', 'duck', 'adgau', ['cuack', 'rap', 'mack', 'vak'])
+];
+
+for (var i =0; i < myFarm.length; i++) {
+    for (var j =0; j < myFarm.length; j++) {
+        if (i !== j) {
+            addFriend(myFarm[i], myFarm[j]);
+        }
+    }
+}
+
+var addMatchesArray = function(farmArray) {
+    for (var i =0; i < farmArray.length; i++) {
+        farmArray[i].matches = [];
+    }
+}
+
+addMatchesArray(myFarm);
+
+//populate the matches propertie randomly
+var giveMatches = function(farmCollection){
+    for (var i = 0; i < farmCollection.length; i++){
+        var index = Math.floor(Math.random()*(farmCollection[i]['friends'].length -0)+0);//canonical form of rendom range of numbers
+        //console.log(index);
+        farmCollection[i].matches.push(farmCollection[i].friends[index]);
+    }
+} 
+
+giveMatches(myFarm);
